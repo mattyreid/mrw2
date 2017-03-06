56404aad9501b8b436610c301917319c2cf1c3b1
mattyreid1 <djlivedtv@gmail.com>
2017-03-06 02:50:30 +0000

    deply 2 up

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
 
   # Define Routes for Pages
   get '/home' => 'pages#home' # override default routes.
-  get '/article' => 'pages#profile'
-  get '/books' => 'pages#explore'
+  get '/How you can achieve financial freedom in 2017' => 'pages#profile'
+  get '/how to live a quality life' => 'pages#explore'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
